845af33065bc8a3e58c267f6ee0a95d68493b6c8
Alec Molloy <alec@kano.me>
2015-07-19 23:20:46 +0100

    basic animation with 3d perlin noise

--- js/app.js
+++ js/app.js
@@ -5,21 +5,19 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 canvas.width = canvas.width * dpr;
 canvas.height = canvas.height * dpr;
-//canvas.width    = 100;
-//canvas.height   = 100;
 canvas.style.width = (canvas.width / dpr) + 'px';
 canvas.style.height = (canvas.height / dpr) + 'px';
 
 var ctx = canvas.getContext('2d');
 
-//drawMultiRadiantCircle(canvas.width / 2, canvas.height / 2, canvas.width/2, colours);
-
-var perlin = new Perlin2D({
+var perlin = new Perlin({
+    dimensions: 3,
     cellSize: 200,
     canvas: canvas,
     ctx: ctx,
     showLattice: false,
-    colour: false
+    colour: false,
+    animation: true
 });
 
 function setupStats() {
@@ -27,7 +25,8 @@ function setupStats() {
     var params = {
         showLattice: perlin.showLattice,
         cellSize: perlin.cellSize,
-        colour: perlin.colour
+        colour: perlin.colour,
+        animating: perlin.animating
     };
     var gui = new dat.GUI({
         height: 5 * 32 - 1
@@ -66,6 +65,12 @@ function setupStats() {
             });
         }); // check if the number of segments needs to be changed}
 
+
 }
 
-    setupStats();
+function animate() {
+    perlin.advance(30);
+    window.requestAnimationFrame(animate);
+}
+
+animate();
